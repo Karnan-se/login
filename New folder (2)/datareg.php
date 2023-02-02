3a713d61444ec30495aa1018f54b3adb85edc9f6
Karnan-se <124253226+Karnan-se@users.noreply.github.com>
2023-02-02 15:26:33 +0530

    Add files via upload

---
+++ New folder (2)/datareg.php
@@ -0,0 +1,22 @@
+<?php
+if(isset($_POST["submit"])){
+    $name=$_POST["fname"];
+    $lname=$_POST["lname"];
+    $comp=$_POST["organisation"];
+    $email=$_POST["email"];
+    $gender=$_POST["gender"];
+    
+    
+    
+}
+echo "$name";
+
+
+
+
+
+
+
+
+
+?>
